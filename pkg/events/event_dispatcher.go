61899cedaeda939cbf7e3630ccb90c8a34ec0a82
Lucassamuel97 <lukassamuka88@gmail.com>
2024-10-03 09:14:17 -0300

    feat: add handler remove method in EventDispatcher

--- pkg/events/event_dispatcher.go
+++ pkg/events/event_dispatcher.go
@@ -49,6 +49,22 @@ func (ed *EventDispatcher) Has(eventName string, handler EventHandlerInterface) 
 	return false
 }
 
+func (ed *EventDispatcher) Remove(eventName string, handler EventHandlerInterface) error {
+	// Verifica se o evento foi registrado
+	if _, ok := ed.handlers[eventName]; ok {
+		// Itera sobre os handlers do evento
+		for i, h := range ed.handlers[eventName] {
+			// Verifica se o handler é o mesmo
+			if h == handler {
+				// Remove o handler do slice
+				ed.handlers[eventName] = append(ed.handlers[eventName][:i], ed.handlers[eventName][i+1:]...)
+				return nil
+			}
+		}
+	}
+	return nil
+}
+
 func (ed *EventDispatcher) Clear() {
 	ed.handlers = make(map[string][]EventHandlerInterface)
 }
